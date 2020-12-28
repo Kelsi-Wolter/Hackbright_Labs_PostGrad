d458f6590936da8143b8ea25d0af86e093382878
Kelsi-Wolter <kelsi.wolter@gmail.com>
2020-12-28 20:55:41 +0000

    Completed challenge 3 in intro_challenges

--- Basic Challenges/intro_challenges.py
+++ Basic Challenges/intro_challenges.py
@@ -73,3 +73,56 @@ def fizzbuzz():
             print('buzz')
         else:
             print(num)
+
+
+# Challenge 3: Longest Word
+# Write a function, find_longest_word, that takes a list of words and returns the length 
+# of the longest one.
+
+# >>> find_longest_word(["hi", "hello"])
+# 5
+
+# >>> find_longest_word(["Balloonicorn", "Hackbright"])
+# 12
+# We’ve given you longest-word.py, which includes the stub of a find_longest_word function:
+
+def find_longest_word(words):
+    """Return longest word in list of words."""
+
+    longest_len = 0
+    longest_word = ''
+
+    for word in words:
+        word_len = len(word)
+        if word_len > longest_len:
+            longest_len = word_len
+            longest_word = word
+    
+    return f'The longest word was {longest_word}, with a length of {longest_len}'
+
+
+# Challenge 4: Decode
+
+# In this challenge, you’ll write a decoder.
+
+# A valid code is a sequence of numbers and letters, always starting with a number and ending with letter(s).
+
+# Each number tells you how many characters to skip before finding a good letter. After each good letter should come the next next number.
+
+# For example, the string “hey” could be encoded by “0h1ae2bcy”. This means “skip 0, find the ‘h’, skip 1, find the ‘e’, skip 2, find the ‘y’”.
+
+# A single letter should work:
+
+# >>> decode("0h")
+# 'h'
+
+# >>> decode("2abh")
+# 'h'
+# Longer patterns should work:
+
+# >>> decode("0h1ae2bcy")
+# 'hey'
+# We’ve provided a file, decode.py, with a stub function in it:
+
+def decode(s):
+    """Decode a string."""
