bbbe3800569e352c162873db95bf5bc28b38a7b2
Kelsi-Wolter <kelsi.wolter@gmail.com>
2021-01-04 18:04:44 +0000

    Completed first 3 and added doctests

--- JavaScript/js-trials-1/trials.py
+++ JavaScript/js-trials-1/trials.py
@@ -2,15 +2,42 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    for item in items:
+        print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    '''Returns list of even numbers in a list
+        >>> get_all_evens([1, 2, 3, 4, 10, 20])
+        [2, 4, 10, 20]
+
+        >>> get_all_evens([1, 3, 5])
+        []    
+    '''
+    even_nums = []
+
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    
+    return even_nums
 
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    '''Returns list of items at odd-number indices from input list
+        >>> get_odd_indices([1, 'hello', true, 500])
+        ['hello', 500]
+        >>> get_odd_indices(['k', 'e', 'l', 's', 'i'])
+        ['e', 's']
+    
+    '''
+    result = []
+
+    for idx in range(len(items)):
+        if idx % 2 != 0:
+            result.append(items[idx])
+    
+    return result
 
 
 def print_as_numbered_list(items):
