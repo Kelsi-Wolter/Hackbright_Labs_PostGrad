4bedeff33af5b57d71f209ac811a7cdf2d3b6a26
Kelsi-Wolter <kelsi.wolter@gmail.com>
2021-01-05 18:11:40 +0000

    Completed functions in functions.js in Javascript trials part 2

--- JavaScript/js-trials-2/functions.js
+++ JavaScript/js-trials-2/functions.js
@@ -3,14 +3,37 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+    town = town.toLowerCase();
+    return town === 'san francisco'
+
+}
 
 
 // 2. getFullName
-
-// Define your function here
+function getFullName(first, last) {
+    return `${first.charAt(0).toUpperCase()}${first.slice(1)} ${last.charAt(0).toUpperCase()}${last.slice(1)}`
+}
 
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;        
+    }
+    else if (state === 'PA') {
+        fee = 2
+    }
+    else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1
+        }
+        else {
+            fee = 3
+        }
+    }
+    console.log(subtotal + fee)
+}
