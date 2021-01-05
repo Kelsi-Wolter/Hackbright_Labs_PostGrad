37a6e011da0c8821ec3d9b89c8e705b8dbd8d6fc
Kelsi-Wolter <kelsi.wolter@gmail.com>
2021-01-05 17:44:01 +0000

    Completed functions in arrays.js Javascript trials part 2

--- JavaScript/js-trials-2/arrays.js
+++ JavaScript/js-trials-2/arrays.js
@@ -3,17 +3,29 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const item in items) {
+    console.log(`${items[item]} ${item}`)
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+  for (const item in items) {
+    if (item % 2 === 0) {
+      result.push(items[item]);
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b);
+  const sortedNItems = sortedItems.slice(0, n);
+  const reversedSortedItems = sortedNItems.reverse();
+
+  console.log(reversedSortedItems);
 }
